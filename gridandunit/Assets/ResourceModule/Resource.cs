d81b93017b517a41085331eec77488e77ce029e7
stefvangils <saa.vangils@student.fontys.nl>
2018-03-09 09:44:37 +0100

    initial commit

---
+++ gridandunit/Assets/ResourceModule/Resource.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Resource : MonoBehaviour
+{
+    public int Quantity;
+    public int InitialQuantity;
+    
+    // Unity Events are a Unity implementation of C#-like events that work with
+    // Unity's serialization system (meaning you can save them in the editor)
+    // Multiple methods can 'Subscribe' to a UnityEvent, and when it is invoked
+    // each of these methods are executed.
+    public UnityEvent OnValueChanged = new UnityEvent(); 
+
+    void Awake()
+    {
+        Quantity = InitialQuantity;
+    }
+    
+    public void AddAmount(int value)
+    {
+        Quantity += value;
+        updateUI();
+    }
+
+    public void RemoveAmount(int value)
+    {
+        Quantity -= value;
+        updateUI();
+    }
+    
+
+    void updateUI()
+    {
+        OnValueChanged.Invoke(); // Invoke the event.
+    }
+}
---
+++ gridandunit/Assets/ResourceModule/ResourceUI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Resource))]
+public class ResourceUI : MonoBehaviour
+{
+    public Text Value;
+    public Text Label;
+
+    private Resource resource;
+
+    private Coroutine valueTicker;
+    private int previousValue;
+    
+    
+    private void Awake()
+    {
+        resource = GetComponent<Resource>();
+        Label.text = resource.name;
+        
+        // Let the method 'updateUI()' run when the OnValueChanged UnityEvent is invoked.
+        // See the resource class for more info.
+        resource.OnValueChanged.AddListener(updateUI); 
+    }
+
+    private void Start()
+    {
+        updateUI();
+    }
+
+    public void updateUI()
+    {
+        
+        // We could just set Value.text to resource.Quantity and be done, but in this
+        // version of the UI we made the visual number to increase until it reaches
+        // the actual number of the model. To do this we use a Coroutine.
+        if (valueTicker != null)
+        {
+            StopCoroutine(valueTicker);
+        }
+        valueTicker = StartCoroutine(TickVisualValue(resource.Quantity));
+    }
+    
+    IEnumerator TickVisualValue(int target)
+    {
+        int current = previousValue;
+        
+        while (current != target)
+        {
+            if (current < target)
+            {
+                current++;
+            }
+            else
+            {
+                current--;
+            }
+            Value.text = current.ToString();
+            // The coroutine will run through this code until it returns a yield.
+            // Then, it will wait until the next frame and continue from where it stopped.
+            // In this case it will change the value by one every frame until current and target are the same.
+            yield return null; 
+        }
+        previousValue = target;
+    }
+}
