e27d9c925f163e0bf0ca28a0015f12f21bb6f249
stefvangils <saa.vangils@student.fontys.nl>
2018-03-09 19:07:39 +0100

    grid made without offset

---
+++ gridandunit/Assets/Scripts/TileGrid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGrid : MonoBehaviour {
+
+    public int xSize;
+    public int ySize;
+    public float offset;
+    public GridTile tile;
+    public Vector3[] gridSize;
+
+
+    // Use this for initialization
+    void Start () {
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Instantiate(tile, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        //}
+        gridSize = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, z = 0; z <= ySize; z++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                gridSize[i] = new Vector3(x, 0, z);
+            }
+        }
+        for (int i = 0; i < gridSize.Length; i++)
+        {
+            Instantiate(tile, gridSize[i], Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
